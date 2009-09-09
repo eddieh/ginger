f1b3ab0d8de14c089c36343b9d5182f0b65b9432
James Dean Palmer <james@tiger3k.com>
2009-09-09 16:26:40 -0700

    Updated license text.

--- src/env.c
+++ src/env.c
@@ -1,23 +1,17 @@
 /*
   Copyright 2009 by James Dean Palmer and others.
+
+  Licensed under the Apache License, Version 2.0 (the "License"); 
+  you may not use this file except in compliance with the License. 
+  You may obtain a copy of the License at 
  
-  Ginger is free software.  You can redistribute and/or modify
-  this work under ther terms of the Apache License Version 2.0
-  or, at your option, Ginger's amended LGPL 2.1 license.  You may
-  not use this file except in compliance with one of these licenses.
-  You may also obtain a copy of the Apache License at
+    http://www.apache.org/licenses/LICENSE-2.0 
  
-    http://www.apache.org/licenses/LICENSE-2.0
- 
-  You may obtain a copy of Ginger's amended LGPL License at:
- 
-    http://ging3r.org/licenses/LGPL-2.1.txt
- 
-  Unless required by applicable law or agreed to in writing, 
-  software distributed under the License is distributed on an 
-  "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, 
-  either express or implied. See the License(s) for the specific 
-  language governing permissions and limitations.
+  Unless required by applicable law or agreed to in writing, software 
+  distributed under the License is distributed on an "AS IS" BASIS, 
+  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. 
+  See the License for the specific language governing permissions and 
+  limitations under the License. 
  
   Please report all bugs and problems to "bugs@ging3r.org".
 */
